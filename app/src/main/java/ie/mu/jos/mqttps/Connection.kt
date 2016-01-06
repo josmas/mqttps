3a8b68203db2d5359275f9864f935dcf31bd5e88
josmas <josmasflores@gmail.com>
2016-01-06 15:34:40 +0000

    App converted to Kotlin automatically (Android Studio)

---
+++ app/src/main/java/ie/mu/jos/mqttps/Connection.kt
@@ -0,0 +1,99 @@
+package ie.mu.jos.mqttps
+
+import android.content.Context
+import android.util.Log
+import android.widget.Toast
+
+import org.eclipse.paho.android.service.MqttAndroidClient
+import org.eclipse.paho.client.mqttv3.IMqttActionListener
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken
+import org.eclipse.paho.client.mqttv3.IMqttToken
+import org.eclipse.paho.client.mqttv3.MqttCallback
+import org.eclipse.paho.client.mqttv3.MqttClient
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions
+import org.eclipse.paho.client.mqttv3.MqttException
+import org.eclipse.paho.client.mqttv3.MqttMessage
+
+/**
+ * A class to create and handle MQTT connections
+ */
+class Connection(private val context: Context) {
+    private var client: MqttAndroidClient? = null
+
+    internal fun connectMqtt() {
+        try {
+            val options = MqttConnectOptions()
+            options.mqttVersion = MqttConnectOptions.MQTT_VERSION_3_1
+
+            val clientId = MqttClient.generateClientId()
+            Log.i(TAG, clientId)
+
+            //TODO (jos) MQTT broker is hardcoded.
+            client = MqttAndroidClient(this.context, "tcp://test.mosquitto.org:1883", clientId)
+
+            if (!client!!.isConnected) {
+                client!!.setCallback(object : MqttCallback {
+
+                    override fun connectionLost(cause: Throwable) {
+                        logFailure("Lost Connection", null, cause)
+                        //TODO (jos) monitor this behaviour - should I connect again?
+                    }
+
+                    @Throws(Exception::class)
+                    override fun messageArrived(topic: String, message: MqttMessage) {
+                        println(topic + ": " + String(message.payload, "UTF-8"))
+                    }
+
+                    override fun deliveryComplete(token: IMqttDeliveryToken) {
+                    }
+                })
+
+                client!!.connect(options, this, object : IMqttActionListener {
+                    override fun onSuccess(asyncActionToken: IMqttToken) {
+                        subscribe("jos_test", 1)
+                    }
+
+                    override fun onFailure(asyncActionToken: IMqttToken, exception: Throwable) {
+                        logFailure("connecting", asyncActionToken, exception)
+                    }
+                })
+
+            }
+
+        } catch (e: MqttException) {
+            e.printStackTrace()
+        }
+
+    }
+
+    private fun logFailure(action: String, asyncActionToken: IMqttToken?, exception: Throwable) {
+        if (asyncActionToken != null) {
+            Toast.makeText(this.context, "Problem: $action to $asyncActionToken", Toast.LENGTH_LONG).show()
+            Log.e(TAG, "Issues $action to $asyncActionToken")
+        } else {
+            Toast.makeText(this.context, "Problem: " + action, Toast.LENGTH_LONG).show()
+        }
+        Log.e(TAG, "Issues " + action + " to " + exception.message)
+    }
+
+    internal fun subscribe(jos_test: String, i: Int) {
+        try {
+            client!!.subscribe("jos_test", 1, null, object : IMqttActionListener {
+                override fun onSuccess(asyncActionToken: IMqttToken) {
+                    Log.i(TAG, "ALL IS GOOD subscribing to " + asyncActionToken)
+                }
+
+                override fun onFailure(asyncActionToken: IMqttToken, exception: Throwable) {
+                    logFailure("subscribing", asyncActionToken, exception)
+                }
+            })
+        } catch (e: MqttException) {
+            e.printStackTrace()
+        }
+
+    }
+
+    companion object {
+        val TAG = "Connection"
+    }
+}
---
+++ app/src/main/java/ie/mu/jos/mqttps/MainActivity.kt
@@ -0,0 +1,53 @@
+package ie.mu.jos.mqttps
+
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+
+        val fab = findViewById(R.id.fab) as FloatingActionButton
+        fab.setOnClickListener { view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show() }
+
+        val connect = findViewById(R.id.connectMqtt) as Button
+        val connection = Connection(this)
+        connect.setOnClickListener { connection.connectMqtt() }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        val id = item.itemId
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    companion object {
+
+        val TAG = "MainActivity"
+    }
+}
